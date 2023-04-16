36df19fdc0d600a1b72a02e5c8d8333cb2226252
Zhicheng HUI <huizhicheng123@gmail.com>
2023-04-16 20:09:28 +0200

    type inference infrastructure

--- INFER.ml
+++ INFER.ml
@@ -54,8 +54,80 @@ type-of (setref exp1 exp2) tenv = void
 
 *)
 
+(* type inference algorithm described in « Essentials of Programming Languages », Chapter 7 *)
 
-"proc (f : ?) proc (x : ?) -((f 3),(f x))"
+let fresh_tvar =
+  let sn = ref 0 in               (* good trick to realize "size effect" (actually not really) *)
+  fun () ->
+    let current_sn = !sn in
+    sn := !sn + 1;
+    TvarType current_sn
+
+let rec otype2type oty =
+  match oty with
+    None -> fresh_tvar ()
+  | Some ty -> ty
+
+(* type environment *)
+type type_environment =
+  | EmptyTEnv
+  | ExtendTEnv of string list * typ list * type_environment
+
+let rec apply_tenv search_name tenv =
+  match tenv with
+    EmptyTEnv -> failwith (Printf.sprintf "Variable \"%s\" is not in the type environment." search_name)
+  | ExtendTEnv (names, tys, saved_tenv) ->
+      let rec loop names tys =
+        match names, tys with
+         [], _ -> apply_tenv search_name saved_tenv
+        | _, [] -> failwith "Names and types lists are not aligned"
+        | name :: names', ty :: tys' -> if name = search_name then ty else loop names' tys'
+      in loop names tys
+
+(* subst, for solving type equations *)
+
+type substitution = (typ * typ) list
+
+let empty_subst () = []
+
+let extend_subst subst tvar tyexp =
+  (tvar, tyexp) :: subst
+
+let rec apply_one_subst tyexp tvar subst =
+  match tyexp with
+    VoidType | IntType | BoolType -> tyexp
+  | ProcType (arg_types, res_type) ->
+      ProcType (List.map (fun x -> apply_one_subst x tvar subst) arg_types, apply_one_subst res_type tvar subst)
+  | RefType tyor ->
+      RefType (apply_one_subst tyor tvar subst)
+  | TvarType sn ->
+      if tyexp = tvar then subst
+      else tyexp
+
+let rec apply_subst2type tyexp (subst : substitution) =
+  match tyexp with
+    VoidType | IntType | BoolType -> tyexp
+  | ProcType (arg_types, res_type) ->
+      ProcType (List.map (fun x -> apply_subst2type x subst) arg_types, apply_subst2type res_type subst)
+  | RefType tyor ->
+      RefType (apply_subst2type tyor subst)
+  | TvarType sn ->
+      let tmp = List.assoc_opt tyexp subst in
+      if tmp <> None then apply_subst2type (Option.get tmp) subst
+      else tyexp
+
+(* free variable check *)
+let rec no_occurrence tvar tyexp =
+  match tyexp with
+    VoidType | IntType | BoolType -> true
+  | ProcType (arg_types, res_type) ->
+      List.for_all (fun x -> no_occurrence tvar x) arg_types && no_occurrence tvar res_type
+  | RefType tyor -> no_occurrence tvar tyor
+  | TvarType sn -> tvar <> tyexp
+
+
+
+"proc (f : ?) proc (x : ?) -((f 3), (f x))"
 
 "letrec
   ? even(odd : ? x : ?) = if zero?(x) then 1 else (odd -(x,1))
